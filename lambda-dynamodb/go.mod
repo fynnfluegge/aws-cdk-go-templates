238a5e8fc653f3bec2b2f234526ad3569bc24948
fynnfluegge <fynnfluegge@gmx.de>
2021-12-04 23:35:25 +0100

    lambda-dynamodb

---
+++ lambda-dynamodb/go.mod
@@ -0,0 +1,11 @@
+module lambda-dynamodb
+
+go 1.16
+
+require (
+	github.com/aws/aws-cdk-go/awscdk v1.134.0-devpreview
+	github.com/aws/aws-lambda-go v1.27.0
+	github.com/aws/aws-sdk-go v1.42.19
+	github.com/aws/constructs-go/constructs/v3 v3.3.161
+	github.com/aws/jsii-runtime-go v1.46.0
+)
---
+++ lambda-dynamodb/lambda-dynamodb.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"github.com/aws/aws-cdk-go/awscdk"
+	"github.com/aws/aws-cdk-go/awscdk/awsdynamodb"
+	"github.com/aws/aws-cdk-go/awscdk/awsiam"
+	"github.com/aws/aws-cdk-go/awscdk/awslambda"
+	"github.com/aws/aws-cdk-go/awscdk/awslambdago"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/constructs-go/constructs/v3"
+	"github.com/aws/jsii-runtime-go"
+)
+
+type LambdaDynamodbStackProps struct {
+	awscdk.StackProps
+}
+
+func NewLambdaDynamodbStack(scope constructs.Construct, id string, props *LambdaDynamodbStackProps) awscdk.Stack {
+	var sprops awscdk.StackProps
+	if props != nil {
+		sprops = props.StackProps
+	}
+	stack := awscdk.NewStack(scope, &id, &sprops)
+
+	dynamoDBRole := awsiam.NewRole(stack, aws.String("myDynamoDBFullAccessRole"), &awsiam.RoleProps{
+		AssumedBy: awsiam.NewServicePrincipal(aws.String("lambda.amazonaws.com"), &awsiam.ServicePrincipalOpts{}),
+		ManagedPolicies: &[]awsiam.IManagedPolicy{
+			awsiam.ManagedPolicy_FromManagedPolicyArn(stack, aws.String("AmazonDynamoDBFullAccess"), aws.String("arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess")),
+		},
+	})
+
+	awslambdago.NewGoFunction(stack, jsii.String("myGoHandler"), &awslambdago.GoFunctionProps{
+		Description: aws.String("myLambdaHandler function"),
+		Runtime:     awslambda.Runtime_GO_1_X(),
+		Entry:       aws.String("./lambda-handler"),
+		Bundling: &awslambdago.BundlingOptions{
+			GoBuildFlags: &[]*string{aws.String(`-ldflags "-s -w"`)},
+		},
+		Role: dynamoDBRole,
+	})
+
+	awsdynamodb.NewTable(stack, jsii.String("myDynamoDB"), &awsdynamodb.TableProps{
+		BillingMode: awsdynamodb.BillingMode_PAY_PER_REQUEST,
+		TableName:   jsii.String("MyDynamoDB"),
+		PartitionKey: &awsdynamodb.Attribute{
+			Name: aws.String("ID"),
+			Type: awsdynamodb.AttributeType_STRING,
+		},
+	})
+
+	return stack
+}
+
+func main() {
+	app := awscdk.NewApp(nil)
+
+	NewLambdaDynamodbStack(app, "LambdaDynamodbStack", &LambdaDynamodbStackProps{
+		awscdk.StackProps{
+			Env: env(),
+		},
+	})
+
+	app.Synth(nil)
+}
+
+// env determines the AWS environment (account+region) in which our stack is to
+// be deployed. For more information see: https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+func env() *awscdk.Environment {
+	// If unspecified, this stack will be "environment-agnostic".
+	// Account/Region-dependent features and context lookups will not work, but a
+	// single synthesized template can be deployed anywhere.
+	//---------------------------------------------------------------------------
+	return nil
+
+	// Uncomment if you know exactly what account and region you want to deploy
+	// the stack to. This is the recommendation for production stacks.
+	//---------------------------------------------------------------------------
+	// return &awscdk.Environment{
+	//  Account: jsii.String("123456789012"),
+	//  Region:  jsii.String("us-east-1"),
+	// }
+
+	// Uncomment to specialize this stack for the AWS Account and Region that are
+	// implied by the current CLI configuration. This is recommended for dev
+	// stacks.
+	//---------------------------------------------------------------------------
+	// return &awscdk.Environment{
+	//  Account: jsii.String(os.Getenv("CDK_DEFAULT_ACCOUNT")),
+	//  Region:  jsii.String(os.Getenv("CDK_DEFAULT_REGION")),
+	// }
+}

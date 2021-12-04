9f739feda64ae32dcce13695838e85c819dacda7
fynnfluegge <fynnfluegge@gmx.de>
2021-12-04 11:39:57 +0100

    sqs-lambda

---
+++ sqs-lambda/go.mod
@@ -0,0 +1,10 @@
+module sqs-lambda
+
+go 1.16
+
+require (
+	github.com/aws/aws-cdk-go/awscdk v1.132.0-devpreview
+	github.com/aws/aws-lambda-go v1.27.0
+	github.com/aws/constructs-go/constructs/v3 v3.3.161
+	github.com/aws/jsii-runtime-go v1.42.0
+)
---
+++ sqs-lambda/sqs-lambda.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"github.com/aws/aws-cdk-go/awscdk"
+	"github.com/aws/aws-cdk-go/awscdk/awslambda"
+	"github.com/aws/aws-cdk-go/awscdk/awslambdaeventsources"
+	"github.com/aws/aws-cdk-go/awscdk/awslambdago"
+	"github.com/aws/aws-cdk-go/awscdk/awssqs"
+	"github.com/aws/constructs-go/constructs/v3"
+	"github.com/aws/jsii-runtime-go"
+)
+
+type SqsLambdaStackProps struct {
+	awscdk.StackProps
+}
+
+func NewSqsLambdaStack(scope constructs.Construct, id string, props *SqsLambdaStackProps) awscdk.Stack {
+	var sprops awscdk.StackProps
+	if props != nil {
+		sprops = props.StackProps
+	}
+	stack := awscdk.NewStack(scope, &id, &sprops)
+
+	queue := awssqs.NewQueue(stack, jsii.String("EventbridgeSqsQueue"), &awssqs.QueueProps{
+		VisibilityTimeout: awscdk.Duration_Seconds(jsii.Number(300)),
+		QueueName:         jsii.String("MySqsQueue"),
+	})
+
+	batchSize := 10.0
+
+	awslambdago.NewGoFunction(stack, jsii.String("myGoHandler"), &awslambdago.GoFunctionProps{
+		Description: jsii.String("QueueConsumerFunction function"),
+		Runtime:     awslambda.Runtime_GO_1_X(),
+		Entry:       jsii.String("./sqs-consumer-handler"),
+		Events: &[]awslambda.IEventSource{
+			awslambdaeventsources.NewSqsEventSource(queue, &awslambdaeventsources.SqsEventSourceProps{
+				BatchSize: &batchSize,
+			}),
+		},
+		Bundling: &awslambdago.BundlingOptions{
+			GoBuildFlags: &[]*string{jsii.String(`-ldflags "-s -w"`)},
+		},
+	})
+
+	awscdk.NewCfnOutput(stack, jsii.String("sqsUrl"), &awscdk.CfnOutputProps{
+		Description: jsii.String("SQS endpoint URL"),
+		Value:       queue.QueueArn(),
+	})
+
+	return stack
+}
+
+func main() {
+	app := awscdk.NewApp(nil)
+
+	NewSqsLambdaStack(app, "SqsLambdaStack", &SqsLambdaStackProps{
+		awscdk.StackProps{
+			Env: env(),
+		},
+	})
+
+	app.Synth(nil)
+}
+
+// env determines the AWS environment (account+region) in which our stack is to
+// be deployed. For more information see: https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+func env() *awscdk.Environment {
+	// If unspecified, this stack will be "environment-agnostic".
+	// Account/Region-dependent features and context lookups will not work, but a
+	// single synthesized template can be deployed anywhere.
+	//---------------------------------------------------------------------------
+	return nil
+
+	// Uncomment if you know exactly what account and region you want to deploy
+	// the stack to. This is the recommendation for production stacks.
+	//---------------------------------------------------------------------------
+	// return &awscdk.Environment{
+	//  Account: jsii.String("123456789012"),
+	//  Region:  jsii.String("us-east-1"),
+	// }
+
+	// Uncomment to specialize this stack for the AWS Account and Region that are
+	// implied by the current CLI configuration. This is recommended for dev
+	// stacks.
+	//---------------------------------------------------------------------------
+	// return &awscdk.Environment{
+	//  Account: jsii.String(os.Getenv("CDK_DEFAULT_ACCOUNT")),
+	//  Region:  jsii.String(os.Getenv("CDK_DEFAULT_REGION")),
+	// }
+}

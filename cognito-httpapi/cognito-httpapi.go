c8669bb1deacb2a8b5e53533b80ec04c40d1ba08
fynnfluegge <fynnfluegge@gmx.de>
2021-12-19 17:49:08 +0100

    cognito-httpapi

---
+++ cognito-httpapi/cognito-httpapi.go
@@ -0,0 +1,142 @@
+package main
+
+import (
+	"github.com/aws/aws-cdk-go/awscdk"
+	"github.com/aws/aws-cdk-go/awscdk/awsapigatewayv2"
+	"github.com/aws/aws-cdk-go/awscdk/awscognito"
+	"github.com/aws/constructs-go/constructs/v3"
+	"github.com/aws/jsii-runtime-go"
+)
+
+type CognitoHttpapiStackProps struct {
+	awscdk.StackProps
+}
+
+func NewCognitoHttpapiStack(scope constructs.Construct, id string, props *CognitoHttpapiStackProps) awscdk.Stack {
+	var sprops awscdk.StackProps
+	if props != nil {
+		sprops = props.StackProps
+	}
+	stack := awscdk.NewStack(scope, &id, &sprops)
+
+	userpool := awscognito.NewUserPool(stack, jsii.String("myCognitoUserPool"), &awscognito.UserPoolProps{
+		UserPoolName: jsii.String("myCognitoUserPool"),
+		PasswordPolicy: &awscognito.PasswordPolicy{
+			MinLength: jsii.Number(8),
+		},
+		AccountRecovery: awscognito.AccountRecovery_EMAIL_ONLY,
+		AutoVerify: &awscognito.AutoVerifiedAttrs{
+			Email: jsii.Bool(true),
+		},
+		StandardAttributes: &awscognito.StandardAttributes{
+			Email: &awscognito.StandardAttribute{
+				Required: jsii.Bool(true),
+				Mutable:  jsii.Bool(false),
+			},
+			EmailVerified: &awscognito.StandardAttribute{
+				Required: jsii.Bool(false),
+				Mutable:  jsii.Bool(true),
+			},
+		},
+		SignInAliases: &awscognito.SignInAliases{
+			Email: jsii.Bool(true),
+		},
+		SelfSignUpEnabled: jsii.Bool(true),
+	})
+
+	awscognito.NewUserPoolClient(stack, jsii.String("MyUserPoolClient"), &awscognito.UserPoolClientProps{
+		UserPoolClientName: jsii.String("MyUserPoolClient"),
+		UserPool:           userpool,
+		GenerateSecret:     jsii.Bool(false),
+		SupportedIdentityProviders: &[]awscognito.UserPoolClientIdentityProvider{
+			awscognito.UserPoolClientIdentityProvider_COGNITO(),
+		},
+		AuthFlows: &awscognito.AuthFlow{
+			UserPassword:      jsii.Bool(true),
+			AdminUserPassword: jsii.Bool(true),
+		},
+		OAuth: &awscognito.OAuthSettings{
+			LogoutUrls:   jsii.Strings("https://oauth.pstmn.io/v1/callback"),
+			CallbackUrls: jsii.Strings("https://oauth.pstmn.io/v1/callback"),
+			Flows: &awscognito.OAuthFlows{
+				AuthorizationCodeGrant: jsii.Bool(true),
+				ImplicitCodeGrant:      jsii.Bool(true),
+			},
+			Scopes: &[]awscognito.OAuthScope{
+				awscognito.OAuthScope_EMAIL(),
+				awscognito.OAuthScope_OPENID(),
+				awscognito.OAuthScope_PROFILE(),
+			},
+		},
+	})
+
+	userPoolDomain := awscognito.NewUserPoolDomain(stack, jsii.String("MyUserPoolDomain"), &awscognito.UserPoolDomainProps{
+		UserPool: userpool,
+		CognitoDomain: &awscognito.CognitoDomainOptions{
+			DomainPrefix: jsii.String("myauth"),
+		},
+	})
+
+	httpApi := awsapigatewayv2.NewHttpApi(stack, jsii.String("MyHttpApi"), &awsapigatewayv2.HttpApiProps{
+		ApiName: jsii.String("MyHttpApi"),
+		CorsPreflight: &awsapigatewayv2.CorsPreflightOptions{
+			AllowMethods: &[]awsapigatewayv2.CorsHttpMethod{
+				awsapigatewayv2.CorsHttpMethod_GET,
+			},
+		},
+	})
+
+	awsapigatewayv2.NewHttpAuthorizer(stack, jsii.String("MyHttpAuthorizer"), &awsapigatewayv2.HttpAuthorizerProps{
+		AuthorizerName: jsii.String("MyHttpAuthorizer"),
+		Type:           awsapigatewayv2.HttpAuthorizerType_JWT,
+		HttpApi:        httpApi,
+		JwtIssuer:      jsii.String("https://cognito-idp.eu-central-1.amazonaws.com/" + *userpool.UserPoolId()),
+		JwtAudience:    jsii.Strings(*userpool.UserPoolId()),
+		IdentitySource: jsii.Strings("$request.header.Authorization"),
+	})
+
+	awscdk.NewCfnOutput(stack, jsii.String("authUrl"), &awscdk.CfnOutputProps{
+		Value: jsii.String("https://" + *userPoolDomain.DomainName() + ".auth.eu-central-1.amazoncognito.com/login"),
+	})
+
+	return stack
+}
+
+func main() {
+	app := awscdk.NewApp(nil)
+
+	NewCognitoHttpapiStack(app, "CognitoHttpapiStack", &CognitoHttpapiStackProps{
+		awscdk.StackProps{
+			Env: env(),
+		},
+	})
+
+	app.Synth(nil)
+}
+
+// env determines the AWS environment (account+region) in which our stack is to
+// be deployed. For more information see: https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+func env() *awscdk.Environment {
+	// If unspecified, this stack will be "environment-agnostic".
+	// Account/Region-dependent features and context lookups will not work, but a
+	// single synthesized template can be deployed anywhere.
+	//---------------------------------------------------------------------------
+	return nil
+
+	// Uncomment if you know exactly what account and region you want to deploy
+	// the stack to. This is the recommendation for production stacks.
+	//---------------------------------------------------------------------------
+	// return &awscdk.Environment{
+	//  Account: jsii.String("123456789012"),
+	//  Region:  jsii.String("us-east-1"),
+	// }
+
+	// Uncomment to specialize this stack for the AWS Account and Region that are
+	// implied by the current CLI configuration. This is recommended for dev
+	// stacks.
+	//---------------------------------------------------------------------------
+	// return &awscdk.Environment{
+	//  Account: jsii.String(os.Getenv("CDK_DEFAULT_ACCOUNT")),
+	//  Region:  jsii.String(os.Getenv("CDK_DEFAULT_REGION")),
+	// }
+}
---
+++ cognito-httpapi/go.mod
@@ -0,0 +1,11 @@
+module cognito-httpapi
+
+go 1.16
+
+require (
+	github.com/aws/aws-cdk-go/awscdk v1.134.0-devpreview
+	github.com/aws/constructs-go/constructs/v3 v3.3.161
+	github.com/aws/jsii-runtime-go v1.46.0
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776 // indirect
+)

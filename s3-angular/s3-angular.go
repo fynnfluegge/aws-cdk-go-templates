3ba03bc325c7cebb0cbdb8254777229727ac8d53
fynnfluegge <fynnfluegge@gmx.de>
2022-05-01 12:54:47 +0200

    s3-angular cloudfront 403 error fix comment added

--- s3-angular/s3-angular.go
+++ s3-angular/s3-angular.go
@@ -43,7 +43,7 @@ func NewS3AngularStack(scope constructs.Construct, id string, props *S3AngularSt
 	bucket := awss3.NewBucket(stack, jsii.String("MyS3Bucket"), &awss3.BucketProps{
 		BucketName:           &props.domain,
 		WebsiteIndexDocument: jsii.String("index.html"),
-		WebsiteErrorDocument: jsii.String("index.html"),
+		WebsiteErrorDocument: jsii.String("index.html"), // this is required since cloudfront throws 403 Error with angular-routing
 		PublicReadAccess:     jsii.Bool(true),
 	})
 

051da48ebcfd40bd365823f2fcbd97a36a4e009e
fynnfluegge <fynnfluegge@gmx.de>
2022-08-21 13:55:25 +0200

    add cognito-httpapi tests

--- cognito-httpapi/cognito-httpapi_test.go
+++ cognito-httpapi/cognito-httpapi_test.go
@@ -13,12 +13,23 @@ func TestCognitoHttpapiStack(t *testing.T) {
 	app := awscdk.NewApp(nil)
 
 	// WHEN
-	stack := NewCognitoHttpapiStack(app, "MyStack", nil)
+	stack := NewCognitoHttpapiStack(app, "MyStack", &CognitoHttpapiStackProps{
+		awscdk.StackProps{
+			Env: env(),
+		},
+	})
 
 	// THEN
 	template := assertions.Template_FromStack(stack)
 
-	template.HasResourceProperties(jsii.String("AWS::SQS::Queue"), map[string]interface{}{
-		"VisibilityTimeout": 300,
+	template.HasResourceProperties(jsii.String("AWS::ApiGatewayV2::Api"), map[string]interface{}{
+		"ProtocolType": "HTTP",
+	})
+
+	template.HasResourceProperties(jsii.String("AWS::Cognito::UserPoolClient"), map[string]interface{}{
+		"CallbackURLs":       jsii.Strings("https://oauth.pstmn.io/v1/callback"),
+		"LogoutURLs":         jsii.Strings("https://oauth.pstmn.io/v1/callback"),
+		"AllowedOAuthFlows":  jsii.Strings("implicit", "code"),
+		"AllowedOAuthScopes": jsii.Strings("email", "openid", "profile"),
 	})
 }
